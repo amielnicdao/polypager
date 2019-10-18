5b465a8a01ffb0559547b396aa69c669f3a36c71
anicdao <amielnicdao@gmail.com>
2019-10-18 15:57:14 -0400

    changed html, added super basic css, added var for what to keep track off, added question and answer array

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,48 @@
-// var of what you need to keep track of
+// var of what I need to keep track of
 // function to reset game
-// function to check if winner got the answer right
+// function to check if winner got the answer right or not
 // function to add to wins or losses or unanswered 
+// some sort of loop or function that goes through each question
+//function to show results
 
+$(document).ready(function () {
+	var questionList = 0;
+	var time = 5;
+	var correct = 0
+	var wrong = 0;
+	var unanswered = 0;
+
+	var questionArray = [
+		{
+			question: "What are the three types of starter Pokemon?",
+			options: ["Grass, Fire, Water", "Grass, Psychic, Fighting", "Lightning, Dark, Fire", "Water, Normal, Grass"],
+			answer: "Grass, Fire, Water",
+			image: "<img src='assets/images/pokeball.jpg' class='img'>"
+		},
+		{
+			question: "In Generation 1, which Pokemon was known for its lethal punches?",
+			options: ["Hitmonlee", "Hitmonchan", "Machamp", "Primeape"],
+			answer: "Hitmonchan",
+			image: "<img src='assets/images/fighting.png' class='img'>"
+		},
+		{
+			question: "What type of attack are flying Pokemon immune to?",
+			options: ["Ground", "Lightning", "Poison", "Dark"],
+			answer: "Ground",
+			image: "img src='assets/images/flying.jpg' class='img'>"
+		},
+		{
+			question: "Which of these is NOT a potential status ailment in a battle?",
+			options: ["Poison", "Sleep", "Confusion", "Faint"],
+			answer: "Faint",
+			image: "<img src='assets/images/status.jpg' class='img'>"
+		},
+		{
+			question: "What's the device trainers use to keep track of their Pokemon encounters?",
+			options: ["Ippo", "Poketrack", "SmartPoke", "Pokedex"],
+			answer: "Pokedex",
+			image: "img src='assets/images/pokedex.png' class='img'>"
+		},
+	]
+
+})
