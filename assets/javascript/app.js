a0b04143e2d817a338880cca372637060991080f
anicdao <amielnicdao@gmail.com>
2019-10-20 00:08:25 -0400

    separated question array to another file, added image to html, more basic styling.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,39 +12,6 @@ $(document).ready(function () {
 	var incorrect = 0;
 	var unanswered = 0;
 
-	var questionArray = [
-		{
-			question: "What are the three types of starter Pokemon?",
-			choices: ["Grass, Fire, Water", "Grass, Psychic, Fighting", "Lightning, Dark, Fire", "Water, Normal, Grass"],
-			answer: "Grass, Fire, Water",
-			image: "<img src='assets/images/pokeball.jpg' class='img'>"
-		},
-		{
-			question: "In Generation 1, which Pokemon was known for its lethal punches?",
-			choices: ["Hitmonlee", "Hitmonchan", "Machamp", "Primeape"],
-			answer: "Hitmonchan",
-			image: "<img src='assets/images/fighting.png' class='img'>"
-		},
-		{
-			question: "What type of attack are flying Pokemon immune to?",
-			choices: ["Ground", "Lightning", "Poison", "Dark"],
-			answer: "Ground",
-			image: "<img src='assets/images/flying.jpg' class='img'>"
-		},
-		{
-			question: "Which of these is NOT a potential status ailment in a battle?",
-			choices: ["Poison", "Sleep", "Confusion", "Faint"],
-			answer: "Faint",
-			image: "<img src='assets/images/status.jpg' class='img'>"
-		},
-		{
-			question: "What's the device trainers use to keep track of their Pokemon encounters?",
-			choices: ["Ippo", "Poketrack", "SmartPoke", "Pokedex"],
-			answer: "Pokedex",
-			image: "<img src='assets/images/pokedex.png' class='img'>"
-		},
-	];
-
 	function qAndA() {
 		$("#game").append("<p><strong>" +
 			questionArray[questionIndex].question +
@@ -131,10 +98,10 @@ $(document).ready(function () {
 	}
 
 	function results() {
-		$("#game").html("<p><strong>Correct: </p>" + correct + 
-			"<p><strong>Incorrect: </p>" + incorrect + 
+		$("#game").html("<p><strong>Correct: </p>" + correct +
+			"<p><strong>Incorrect: </p>" + incorrect +
 			"<p><strong>Unanswered: </p>" + unanswered);
-		$("#game").append("<h1 id='startGame'>Another Round?</h1>");
+		$("#game").append("<h1 id='startGame'>Another Round?</h1>")
 		$("#startGame").click(nextQuestion);
 		gameReset();
 	}
---
+++ assets/javascript/questions.js
@@ -0,0 +1,32 @@
+var questionArray = [
+    {
+        question: "What are the three types of starter Pokemon?",
+        choices: ["Grass, Fire, Water", "Grass, Psychic, Fighting", "Lightning, Dark, Fire", "Water, Normal, Grass"],
+        answer: "Grass, Fire, Water",
+        image: "<img src='assets/images/pokeball.jpg' class='img'>"
+    },
+    {
+        question: "In Generation 1, which Pokemon was known for its lethal punches?",
+        choices: ["Hitmonlee", "Hitmonchan", "Machamp", "Primeape"],
+        answer: "Hitmonchan",
+        image: "<img src='assets/images/fighting.png' class='img'>"
+    },
+    {
+        question: "What type of attack are flying Pokemon immune to?",
+        choices: ["Ground", "Lightning", "Poison", "Dark"],
+        answer: "Ground",
+        image: "<img src='assets/images/flying.jpg' class='img'>"
+    },
+    {
+        question: "Which of these is NOT a potential status ailment in a battle?",
+        choices: ["Poison", "Sleep", "Confusion", "Faint"],
+        answer: "Faint",
+        image: "<img src='assets/images/status.jpg' class='img'>"
+    },
+    {
+        question: "What's the device trainers use to keep track of their Pokemon encounters?",
+        choices: ["Ippo", "Poketrack", "SmartPoke", "Pokedex"],
+        answer: "Pokedex",
+        image: "<img src='assets/images/pokedex.png' class='img'>"
+    },
+];
